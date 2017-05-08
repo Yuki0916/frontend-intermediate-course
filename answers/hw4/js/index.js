3180d476be18099c381e9744c2c25f1faee060c6
Sin-Han,Jhu <blueice0916@gmail.com>
2017-05-08 11:06:38 +0800

    [HW4] 新增作業
    作業4：從假資料到真資料：Ajax 與 API 串接
    .channel_img內的background圖片屬性重寫

---
+++ answers/hw4/js/index.js
@@ -0,0 +1,46 @@
+$(function() {
+  $.ajax({
+    headers: {
+      "Client-ID": "y7kmxph29brxczhdk9dn9ardapegmd"
+    },
+    type: "GET",
+    url: "https://api.twitch.tv/kraken/streams/?game=League%20of%20Legends&limit=20",
+    success: function(data) {
+      for (var c = 0; c < data.streams.length; c++) {
+        $("div#" + c + ".channel" + ">" + "div.channel_img").css(
+          "background-image",
+          "url(" + data.streams[c].preview.medium + ")"
+        );
+        $("div#" + c + ".channel" + ">" + "div.channel_img").css(
+          "background-size",
+          "100% 100%"
+        );
+        $(
+          "div#" +
+            c +
+            ".channel" +
+            ">" +
+            "div.user" +
+            ">" +
+            "div.user_info" +
+            ">" +
+            "div.user_title"
+        ).append(data.streams[c].channel.status);
+        $(
+          "div#" +
+            c +
+            ".channel" +
+            ">" +
+            "div.user" +
+            ">" +
+            "div.user_info" +
+            ">" +
+            "div.user_name"
+        ).append(data.streams[c].channel.name);
+      } // 依序印出所有頻道資訊
+    },
+    error: function() {
+      console.log("fail");
+    }
+  });
+});
