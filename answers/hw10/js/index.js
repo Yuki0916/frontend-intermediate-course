f29c5aa75c7d89006c69cd51929271507f62d3d5
Sin-Han,Jhu <blueice0916@gmail.com>
2017-12-19 11:42:03 +0800

    [HW10] 使用standard --fix修正

--- answers/hw10/js/index.js
+++ answers/hw10/js/index.js
@@ -1,33 +1,33 @@
-var game = "League%20of%20Legends";
-var client_ID = "y7kmxph29brxczhdk9dn9ardapegmd";
-var limit = 20;
-var offset = 0;
-var lang = "zh-tw";
+var game = 'League%20of%20Legends'
+var client_ID = 'y7kmxph29brxczhdk9dn9ardapegmd'
+var limit = 20
+var offset = 0
+var lang = 'zh-tw'
 
-renderChannel(limit);
-xhr(url(client_ID, limit, offset, lang), offset);
+renderChannel(limit)
+xhr(url(client_ID, limit, offset, lang), offset)
 
-function remove(){
-  var body = document.querySelector(".flex-container");
-  body.innerHTML = '';
+function remove () {
+  var body = document.querySelector('.flex-container')
+  body.innerHTML = ''
 }
 
-function i18n(_lang){
-  var body = document.querySelectorAll(".switch h1");
-  body["0"].textContent = window.I18N[_lang].TITLE;
-  lang = _lang ;
-  remove();
-  renderChannel(limit);
-  offset = 0 ; //初始化
-  xhr(url(client_ID, limit, offset, lang), offset);
+function i18n (_lang) {
+  var body = document.querySelectorAll('.switch h1')
+  body['0'].textContent = window.I18N[_lang].TITLE
+  lang = _lang
+  remove()
+  renderChannel(limit)
+  offset = 0  // 初始化
+  xhr(url(client_ID, limit, offset, lang), offset)
 }
-function renderChannel(limit) {
-  var flex_container = document.querySelector(".flex-container");
+function renderChannel (limit) {
+  var flex_container = document.querySelector('.flex-container')
   for (var c = 0; c < limit; c++) {
-    flex_container.innerHTML += getChannel();
+    flex_container.innerHTML += getChannel()
   }
 }
-function getChannel() {
+function getChannel () {
   return `
   <div class="channel">
     <div class="img">
@@ -42,74 +42,74 @@ function getChannel() {
         <div class="user_name"></div>
       </div>
     </div>
-  </div>`;
+  </div>`
 }
-function xhr(url, offset) {
-  var httpRequest;
-  //build XMLHttpRequest
+function xhr (url, offset) {
+  var httpRequest
+  // build XMLHttpRequest
   if (window.XMLHttpRequest) {
-    httpRequest = new XMLHttpRequest();
+    httpRequest = new XMLHttpRequest()
   } else {
     try {
-      httpRequest = new ActiveXObject();
+      httpRequest = new ActiveXObject()
     } catch (e) {
-      console("XMLHttpRequest not supported");
+      console('XMLHttpRequest not supported')
     }
   }
-  //Request Start
-  httpRequest.open("GET", url);
-  httpRequest.onload = function() {
+  // Request Start
+  httpRequest.open('GET', url)
+  httpRequest.onload = function () {
     if (httpRequest.status >= 200 && httpRequest.status < 400) {
       // status success!
       // JSON
-      var data = JSON.parse(httpRequest.response);
-      //run
-      dataIntoModel(data, offset);
+      var data = JSON.parse(httpRequest.response)
+      // run
+      dataIntoModel(data, offset)
     } else {
       // We reached our target server, but it returned an error
     }
-  };
-  httpRequest.onerror = function() {
+  }
+  httpRequest.onerror = function () {
     // There was a connection error of some sort
-  };
-  httpRequest.send();
+  }
+  httpRequest.send()
 }
-function dataIntoModel(data, offset) {
-  var channel_img = document.querySelectorAll(".channel_img");
-  var channel_logo = document.querySelectorAll(".user_img");
-  var channel_title = document.querySelectorAll(".user_title");
-  var channel_name = document.querySelectorAll(".user_name");
+function dataIntoModel (data, offset) {
+  var channel_img = document.querySelectorAll('.channel_img')
+  var channel_logo = document.querySelectorAll('.user_img')
+  var channel_title = document.querySelectorAll('.user_title')
+  var channel_name = document.querySelectorAll('.user_name')
   for (var c = 0; c < data.streams.length; c++) {
     channel_img[c + offset].addEventListener(
-      "load",
-      function() {
-        this.style.opacity = 1;
+      'load',
+      function () {
+        this.style.opacity = 1
       },
       true
-    );
-    channel_img[c + offset].src = data.streams[c].preview.medium;
+    )
+    channel_img[c + offset].src = data.streams[c].preview.medium
     channel_logo[c + offset].addEventListener(
-      "load",
-      function() {
-        this.style.opacity = 1;
+      'load',
+      function () {
+        this.style.opacity = 1
       },
       true
-    );
-    channel_logo[c + offset].src = data.streams[c].channel.logo;
-    channel_title[c + offset].innerHTML = data.streams[c].channel.status;
-    channel_name[c + offset].innerHTML = data.streams[c].channel.display_name;
+    )
+    channel_logo[c + offset].src = data.streams[c].channel.logo
+    channel_title[c + offset].innerHTML = data.streams[c].channel.status
+    channel_name[c + offset].innerHTML = data.streams[c].channel.display_name
   }
 }
-window.addEventListener("scroll", function(event) {
+window.addEventListener('scroll', function (event) {
   if (
     window.pageYOffset + window.innerHeight >
     document.body.scrollHeight - 200
   ) {
-    renderChannel(limit);
-    offset += limit;
-    xhr(url(client_ID, limit, offset, lang), offset);
+    renderChannel(limit)
+    offset += limit
+    xhr(url(client_ID, limit, offset, lang), offset)
   }
-});
-function url(client_ID, limit, offset, lang) {
-  return `https://api.twitch.tv/kraken/streams/?client_id=${client_ID}&game=${game}&limit=${limit}&offset=${offset}&language=${lang}`;
+})
+function url (client_ID, limit, offset, lang) {
+  return `https://api.twitch.tv/kraken/streams/?client_id=${client_ID}&game=${game}&limit=${limit}&offset=${offset}&language=${lang}`
 }
--- answers/hw10/js/lang-zh-tw.js
+++ answers/hw10/js/lang-zh-tw.js
@@ -1,4 +1,4 @@
-if (!window.I18N) window.I18N = {};
+if (!window.I18N) window.I18N = {}
 window.I18N['zh-tw'] = {
   TITLE: '用中文直播的頻道'
 }
